a544038aad72a7bdbf6a85471bd72e8a66c63159
higorj <higor_sgomes@outlook.com>
2022-10-23 12:48:48 -0300

    Ajuste deploy - script

--- data-source.ts
+++ data-source.ts
@@ -48,6 +48,7 @@ export const AppDataSource = new DataSource({
   ],
    migrations: ['src/database/migrations/*{.ts,.js}']
 })
+
 AppDataSource.initialize()
     .then(() => {
       console.log("Data Source has been initialized!")
